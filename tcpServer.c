41346dacba59deb0caa35090c7293f40c5724f47
2017029425 <sunung007@hanyang.ac.kr>
2018-11-29 21:38:00 +0900

    window

--- tcpServer.c
+++ tcpServer.c
@@ -7,11 +7,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <winsock.h>
 
 
 int main() {
-    SOCKET sock, 
+	SOCKET sock, clinetSock;
+
+	int serv_sd;
+	int clnt_sd;
+	int fd;
+	char buf[BUFSIZE];
+
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	int clnt_addr_size;
+	int len;
+
+	fd = open("file_server.c", O_RDONLY); //ÆÄÀÏ¿ÀÇÂ
+
+	serv_sd = socket(PF_INET, SOCK_STREAM, 0);
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	bind(serv_sd, ( struct sockaddr * )&serv_addr,
+		 sizeof(serv_addr));
+	listen(serv_sd, 5);
 
 
 
