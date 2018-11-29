32026b52eea41711ebee6941a64438cbe1eb7ec2
2017029425 <2017029425@hanyang.ac.kr>
2018-11-29 21:19:09 +0900

    start

---
+++ tcpServer.c
@@ -0,0 +1,20 @@
+// 2017029425 Kim Sun Woong
+// Computer software engineering department, Hanyang univ.
+
+// tcpServer.c
+// Server in TCP, TCP/IP protocol
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+int main() {
+    SOCKET sock, 
+
+
+
+
+    return 0;
+}
